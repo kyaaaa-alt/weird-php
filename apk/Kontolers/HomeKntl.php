b1bcb9b281f7ef054b914fb27186c8fba6eec5f3
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-09-24 19:07:41 +0700

    Update HomeKntl.php

--- apk/Kontolers/HomeKntl.php
+++ apk/Kontolers/HomeKntl.php
@@ -4,11 +4,11 @@ use Apk\Modols\HomeModol;
 
 class HomeKntl {
     public function __construct() {
-        $this->suUsersM = new HomeModol();
+        $this->HomeModol = new HomeModol();
     }
 
     public function index() {
-//        $get_user = $this->suUsersM->get_users_active();
+//        $get_user = $this->HomeModol->get_users_active();
 //        dd($get_user); // use d() or dd() for dump!
         $data['title'] = 'Hello World';
         $data['subtitle'] = 'Kyaaaa Weird PHP Framework';
